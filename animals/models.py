3c9739ea940bd59fe1d80eab6cbc692b04a87f9c
matheusprado1 <matheusprado1096@gmail.com>
2022-08-09 14:20:12 -0300

    feat: update models and generate migrations

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Sex(models.TextChoices):
+    MALE = ("Macho",)
+    FEMALE = ("Fêmea",)
+    DEFAULT = ("Não informado",)
+
+
+class Animal(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    weight = models.FloatField()
+    sex = models.CharField(
+        max_length=15, choices=Sex.choices, default=Sex.DEFAULT)
+
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.CASCADE, related_name="animals"
+    )
+
+    traits = models.ManyToManyField(
+        "traits.Trait", related_name="animals"
+    )
+
+    def __repr__(self) -> str:
+        return f"{self.name} - {self.age} - {self.weight} - {self.sex}"
--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    scientific_name = models.CharField(max_length=50, unique=True)
+
+    def __repr__(self) -> str:
+        return f"{self.name} - {self.scientific_name}"
